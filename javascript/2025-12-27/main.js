7b7e32a221c85d4066e045ebb04d42a96dcc58eb
ADMIN <duongduy7070@gmail.com>
2025-12-29 18:15:54 +0700

    09-12-2025

---
+++ javascript/2025-12-27/main.js
@@ -0,0 +1,72 @@
+// console.log("hello anh em")
+// console.log("ok anh em")
+// console.log("1000")
+// console.log(1000, 2000, "OK ANH EM")
+// console.log('1000', 2000, 'OK ANH EM')
+// console.log(1000, 2000, `OK ANH EM`)
+//
+// console.log(as912wdqs)
+
+// khai bao bien (declare)
+// let x, y, z
+// x = "ok anh em"
+//
+// // khop tao bien (init)
+// const a = 1
+// let b = 2
+// var c = 3
+//
+//
+// console.log(a, b, c, x, y, z)
+//
+// let a = 1   // init
+//
+// a = 2
+
+// let a = 100
+// console.log(a)
+// a = String(a)
+// console.log(a)
+//
+// let b = "100"
+// console.log(b)
+// b = Number(b)
+// console.log(b)
+
+// let isNumber = true
+// console.log(isNumber)
+// isNumber = Number(isNumber)
+// console.log(isNumber)
+
+// let a = 1000
+// console.log(a)
+// a = Boolean(a)
+// console.log(a)
+
+// let isNumber = ""
+// console.log(isNumber)
+// isNumber = Boolean(isNumber)
+// console.log(isNumber)
+
+
+// const numbers = [1, 2, 3]
+// console.log(numbers)
+// const names = ["huyvd", "minhk"]
+// console.log(names)
+
+// const numbers = [
+//   [[1, 2, 1], [2, 3, 1]],
+//   [[1, 2, 1], [2, 3, 2]],
+//   [[1, 2, 2], [2, 3, 3], 2]
+// ]
+// console.log(numbers)
+
+// const dict = {
+//   id: 1,
+//   name: "test"
+// }
+// console.log(dict)
+
+const numbers = [4, 3, 1, 5, 1]
+console.log(numbers[0])
+console.log(numbers[2])
